c2d9ba8a4e0b6d7eaa24e598c9bb5f10a0822cad
harumaxy <harumaxy@gmail.com>
2024-02-10 16:00:16 +0900

    fix warning

--- crates/zed/src/languages/gdscript.rs
+++ crates/zed/src/languages/gdscript.rs
@@ -1,16 +1,8 @@
-use anyhow::{anyhow, Context, Result};
-use async_compression::futures::bufread::GzipDecoder;
-use async_tar::Archive;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use futures::{io::BufReader, StreamExt};
 use language::{LanguageServerName, LspAdapter, LspAdapterDelegate};
 use lsp::LanguageServerBinary;
-use smol::fs;
-use std::env::consts::ARCH;
 use std::{any::Any, path::PathBuf};
-use util::async_maybe;
-use util::github::latest_github_release;
-use util::{github::GitHubLspBinaryVersion, ResultExt};
 
 pub struct GodotLspAdapter;
 
@@ -26,23 +18,23 @@ impl LspAdapter for GodotLspAdapter {
 
     async fn fetch_latest_server_version(
         &self,
-        delegate: &dyn LspAdapterDelegate,
+        _delegate: &dyn LspAdapterDelegate,
     ) -> Result<Box<dyn 'static + Send + Any>> {
         Ok(Box::new(()))
     }
 
     async fn fetch_server_binary(
         &self,
-        version: Box<dyn 'static + Send + Any>,
-        container_dir: PathBuf,
-        delegate: &dyn LspAdapterDelegate,
+        _version: Box<dyn 'static + Send + Any>,
+        _container_dir: PathBuf,
+        _delegate: &dyn LspAdapterDelegate,
     ) -> Result<LanguageServerBinary> {
         Err(anyhow!("Godot editor must be installed manually"))
     }
 
     async fn cached_server_binary(
         &self,
-        container_dir: PathBuf,
+        _container_dir: PathBuf,
         _: &dyn LspAdapterDelegate,
     ) -> Option<LanguageServerBinary> {
         None
@@ -50,7 +42,7 @@ impl LspAdapter for GodotLspAdapter {
 
     async fn installation_test_binary(
         &self,
-        container_dir: PathBuf,
+        _container_dir: PathBuf,
     ) -> Option<LanguageServerBinary> {
         None
     }
